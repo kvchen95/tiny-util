feddb9fa2bb553c6d0f30545751c27eae5c6b560
kvchen95 <kvchen95@outlook.com>
2022-08-18 23:36:14 +0800

    feat:

---
+++ 001.query-string.js
@@ -0,0 +1,13 @@
+// 实现 url 参数 与 对象相互转换
+
+function obj2str(obj) {
+  return Object.keys(obj).map(k => `${k}=${obj[k]}`).join('&')
+}
+
+obj2str({ a: '122', d: 'werwe' }) // a=122&d=werwe
+
+function str2obj(str) {
+  return Object.fromEntries(str.split('&').map(col => col.split('=')))
+}
+
+str2obj('a=122&d=werwe'); // { a: '122', d: 'werwe' }
