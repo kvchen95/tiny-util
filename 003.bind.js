7662b9bbd666b0549b752b2959a15151a755326f
gang.chen1 <gang.chen1@yintech.cn>
2022-08-22 19:10:14 +0800

    feat: bind

--- 003.bind.js
+++ 003.bind.js
@@ -1,4 +1,4 @@
-// bind call apply 方法作用一致，都是改变 this 指向的 不同的是 call apply 立即执行 bing 返回改变 this 指向后的新函数 
+// bind call apply 方法作用一致，都是改变 this 指向的 不同的是 call apply 立即执行,而 bind 返回改变 this 指向后的新函数 
 // bind() 方法创建一个新的函数，在 bind() 被调用时，这个新函数的 this 被指定为 bind() 的第一个参数，而其余参数将作为新函数的参数，供调用时使用。
 
 
