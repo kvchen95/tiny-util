972397e77491d58c6b88d1de51a8ff0c115cd880
kvchen95 <kvchen95@outlook.com>
2022-09-10 01:32:13 +0800

    feat: lazy-man

---
+++ 011.lazy-man.js
@@ -0,0 +1,21 @@
+// new Piggy().eat('breakfust').sleep(3).eat('luanch').sleep(5).eat('dinner')
+// log('breakfust')
+// timeout 3s 
+// log('launch')
+// timeout 5 s
+// log('dinner')
+
+
+class Piggy {
+  constructor() {
+
+  }
+
+  eat(meal) {
+    return this
+  }
+
+  sleep(time) {
+    return this
+  }
+}
