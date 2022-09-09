a409a25154587d8ccd493e04d3c0b89eb55a9944
kvchen95 <kvchen95@outlook.com>
2022-09-10 01:43:20 +0800

    feat: lazy-man

--- 011.lazy-man.js
+++ 011.lazy-man.js
@@ -7,15 +7,44 @@
 
 
 class Piggy {
+  tasks = []
   constructor() {
-
+    // after tasks setted start one
+    setTimeout(() => {
+      this.next()
+    })
   }
 
   eat(meal) {
+    const task  = () => {
+      return new Promise((resolve) => {
+        console.log(meal)
+        resolve()
+      })
+    }
+    this.tasks.push(task)
     return this
   }
 
   sleep(time) {
+    const task  = () => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve()
+        }, time * 1000)
+      })
+      
+    }
+    this.tasks.push(task)
     return this
   }
+
+  next() {
+    const task = this.tasks.shift()
+    task && task().then(() => {
+      this.next()
+    })
+  }
 }
+
+new Piggy().eat('breakfust').sleep(3).eat('luanch').sleep(5).eat('dinner')
